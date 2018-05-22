c52ef33c837515e6ff4a6d15af574b7bc59c1552
douasin <ray92105@gmail.com>
2018-05-22 14:17:51 +0800

    step3: Implement the button row(can't run yet)

--- lib/main.dart
+++ lib/main.dart
@@ -54,5 +54,63 @@ class MyApp extends StatelessWidget {
         ],
       ),
     );
+
+ 
+    // Since the code for building each row would be almost identical,
+    // it's most efficient to create a nested function, such as
+    // `buildButtonColumn()`, which takes an Icon and Text,
+    // and returns a column with its widgets painted in the primary color.
+    Column buildButtonColumn(IconData icon, String label) {
+      Color color = Theme.of(context).primaryColor;
+
+      return new Column(
+          mainAxisSize: MainAxisSize.min,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            new Icon(icon, color: color),
+            new Container(
+              margin: const EdgeInsets.only(top: 8.0),
+              child: new Text(
+                label,
+                style: new TextStyle(
+                  fontSize: 12.0,
+                  fontWeight: FontWeight.w400,
+                  color: color,
+                ),
+              ),
+            ),
+          ],
+      );
+    }
+    // The build function adds the icon directly to the column.
+    // Put text into a Container to add padding above the text,
+    // separating it from the icon.
+    //
+    // Build the row containing these columns by calling the function
+    // and passing the icon and text specific to that column.
+    // Align the columns along the main using MainAxisAlignment.spaceEvenly
+    // to arrange the free space evenly before, between, and after each column.
+    Widget buttonSection = new Container(
+      child: new Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          buildButtonColumn(Icons.call, 'CALL'),
+          buildButtonColumn(Icons.near_me, 'ROUTE'),
+          buildButtonColumn(Icons.share, 'SHARE'),
+        ],
+      ),
+    );
+
+    // The button section contains 3 columns that use the same layout
+    //   - an icon over a row of text.
+    // The columns in this row are evenly spaces, and the text
+    // and icons are painted with the primary color, which is set to
+    // blue in the app's build() method;
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+    ),
   }
 }
