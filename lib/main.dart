ea7fbc1a52e51bf94f1b68370818b606d2f0e59b
douasin <ray92105@gmail.com>
2018-05-22 14:29:59 +0800

    step5: Implement the image section

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,6 @@ class MyApp extends StatelessWidget {
       ),
     );
 
- 
     // Since the code for building each row would be almost identical,
     // it's most efficient to create a nested function, such as
     // `buildButtonColumn()`, which takes an Icon and Text,
@@ -64,24 +63,25 @@ class MyApp extends StatelessWidget {
       Color color = Theme.of(context).primaryColor;
 
       return new Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            new Icon(icon, color: color),
-            new Container(
-              margin: const EdgeInsets.only(top: 8.0),
-              child: new Text(
-                label,
-                style: new TextStyle(
-                  fontSize: 12.0,
-                  fontWeight: FontWeight.w400,
-                  color: color,
-                ),
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          new Icon(icon, color: color),
+          new Container(
+            margin: const EdgeInsets.only(top: 8.0),
+            child: new Text(
+              label,
+              style: new TextStyle(
+                fontSize: 12.0,
+                fontWeight: FontWeight.w400,
+                color: color,
               ),
             ),
-          ],
+          ),
+        ],
       );
     }
+
     // The build function adds the icon directly to the column.
     // Put text into a Container to add padding above the text,
     // separating it from the icon.
@@ -105,13 +105,13 @@ class MyApp extends StatelessWidget {
     // along each edge. The `softwrap` property indicates wheter
     // the text should break on soft link breaks, such as periods or commons.
     Widget textSection = new Container(
-        padding: const EdgeInsets.all(32.0),
-        child: new Text(
-          '''
+      padding: const EdgeInsets.all(32.0),
+      child: new Text(
+        '''
 Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situated 1,578 meters above sea level, it is one of the larger Alpine Lakes. A gondola ride from Kandersteg, followed by a half-hour walk through pastures and pine forest, leads you to the lake, which warms to 20 degrees Celsius in the summer. Activities enjoyed here include rowing, and riding the summer toboggan run.
           ''',
-          softWrap: true,
-        ),
+        softWrap: true,
+      ),
     );
 
     // The button section contains 3 columns that use the same layout
@@ -124,6 +124,20 @@ Lake Oeschinen lies at the foot of the Blüemlisalp in the Bernese Alps. Situate
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-    ),
+      // Tree of the four column elements are now copmlete, leaving only
+      // the image. This image is availabel online under the Creative
+      // Commons license, but it's arge and slow to fetch. In `Step 0`
+      // you included the image in the project and updated the
+      // `pubspec file`, so you can now reference it from your code.
+      body: new ListView(
+        children: [
+          new Image.asset(
+            'images/lake.jpg',
+            height: 240.0,
+            fit: Boxfit.cover,
+          ),
+        ],
+      ),
+    );
   }
 }
